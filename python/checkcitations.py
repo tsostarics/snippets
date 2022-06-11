9504ea38b5b7ddacf15363e5d591e58c58359112
Thomas Sostarics <tsostarics@gmail.com>
2022-06-10 22:02:11 -0500

    Add check for bad labels, which cause undefined citation issues that stop compilation

--- python/checkcitations.py
+++ python/checkcitations.py
@@ -19,23 +19,31 @@ if __name__ == "__main__":
   
   bib_pat = re.compile(r"@[^{]+{([^,]+)")
   cite_pat = re.compile(r"\\(cite|parencite|textcite)\{([^}]+)\}")
-    
-  bib_entries = defaultdict(int)
-  intext_citations = defaultdict(list) # Map example strings to their example index
-  
+  badlabel_pat = re.compile(r"(\\label|\\ref)\{([^}_]*_[^}_]*)\}")
+  bib_entries = defaultdict(int) # Map bib keys to bib file line
+  intext_citations = defaultdict(list) # Map citation keys to tex file lines
+  intext_bad_labels = defaultdict(list) # Map citation keys to tex file lines
+
   with open(texfile, 'r') as f:
     for line in f:
       line_count += 1
       
       # Extract citation calls (\cite, \parencite, \textcite) from the line
       citation_args = cite_pat.findall(line)
-      
       if len(citation_args) != 0: # If we found any citations
         for arg_tuple in citation_args:
           citations = arg_tuple[1].split(",") # Split up any multi citations
           for cite in citations:
             intext_citations[cite.strip()].append(line_count)
-  
+      
+      # Check if there are any bad labels containing underscores,
+      # which will cause misleading undefined citation errors
+      bad_labels = badlabel_pat.findall(line)
+      if len(bad_labels) != 0: 
+        for label_tuple in bad_labels:
+          bad_label = label_tuple[1]
+          intext_bad_labels[bad_label].append(line_count)
+      
   line_count = 0
   
   with open(bibfile, 'r',encoding='utf8') as f:
@@ -51,6 +59,10 @@ if __name__ == "__main__":
   text_set = {entry for entry in intext_citations.keys()}
   typos = text_set.difference(bib_set)
   
+  print(f" - bib file contains {len(bib_set)} references")
+  print(f" - tex file contains {len(text_set)} unique citations")
+  print(f" - tex file contains {len(intext_bad_labels)} bad labels")
+  
   print("Here are the potential typos and the lines they happen on:")
   for typo in typos:
     print(f"\t{typo}: {intext_citations[typo]}")
@@ -58,3 +70,8 @@ if __name__ == "__main__":
   print("Here are the citations with entries but are not used:")
   for unused in bib_set.difference(text_set):
     print(f"\t{unused}: {bib_entries[unused]}")
+
+  if len(intext_bad_labels) != 0:
+    print("Undefined citation issues caused by these labels with underscores:")
+    for k in intext_bad_labels:
+      print(f"{k}: {intext_bad_labels[k]}")
